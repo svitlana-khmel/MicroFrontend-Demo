605f913937609a947c0e43c64b06bee55f535719
Svitlana Khmel <>
2025-10-14 23:44:55 +0200

    no need in craco.config

--- host-app/craco.config.js
+++ host-app/craco.config.js
@@ -1,21 +1,21 @@
-const { ModuleFederationPlugin } = require("webpack").container;
+// const { ModuleFederationPlugin } = require("webpack").container;
 
-module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.plugins.push(
-        new ModuleFederationPlugin({
-          name: "Host",
-          remotes: {
-            MicroFrontend: "MicroFrontend@http://localhost:3000/remoteEntry.js",
-          },
-          shared: {
-            react: { singleton: true, requiredVersion: "18.2.0" },
-            "react-dom": { singleton: true, requiredVersion: "18.2.0" },
-          },
-        })
-      );
-      return webpackConfig;
-    },
-  },
-};
+// module.exports = {
+//   webpack: {
+//     configure: (webpackConfig) => {
+//       webpackConfig.plugins.push(
+//         new ModuleFederationPlugin({
+//           name: "Host",
+//           remotes: {
+//             MicroFrontend: "MicroFrontend@http://localhost:3000/remoteEntry.js",
+//           },
+//           shared: {
+//             react: { singleton: true, requiredVersion: "18.2.0" },
+//             "react-dom": { singleton: true, requiredVersion: "18.2.0" },
+//           },
+//         })
+//       );
+//       return webpackConfig;
+//     },
+//   },
+// };
